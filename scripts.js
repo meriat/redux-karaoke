23cc4f78b8f86c6c9593e18d1ccd7b2812919de0
Meria Thomas <josemeria93@gmail.com>
2018-12-03 14:16:30 -0800

    display song lyrics for one song

--- scripts.js
+++ scripts.js
@@ -1,3 +1,85 @@
-const userClick = () => {
-  console.log('click');
+// LYRIC INFO
+const songList = {
+  1: "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', '),
+  2: "Twenty-five years and my life is still, Trying to get up that great big hill of hope, For a destination, I realized quickly when I knew I should, That the world was made up of this brotherhood of man, For whatever that means, And so I cry sometimes when I'm lying in bed, Just to get it all out what's in my head, And I, I am feeling a little peculiar, And so I wake in the morning and I step outside, And I take a deep breath and I get real high, and I Scream from the top of my lungs, What's going on?, And I say hey yeah yeah hey yeah yeah, I said hey what's going on?, And I say hey yeah yeah hey yeah yeah,I said hey what's going on?".split(', ')
+};
+
+//INITIAL REDUX STATE
+const initialState = {
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0,
 }
+
+//REDUX REDUCER
+const reducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case 'NEXT_LYRIC': 
+      let newArrayPosition = state.arrayPosition + 1;
+      newState = {
+        songLyricsArray: state.songLyricsArray,
+        arrayPosition: newArrayPosition
+      }
+      return newState;
+
+    case 'RESTART_SONG':
+      newState = initialState;
+      return newState;
+
+    default: return state;
+  }
+}
+
+// JEST TESTS + SETUP
+const { expect } = window;
+
+expect(reducer(initialState, { type: null})).toEqual(initialState);
+
+expect(reducer(initialState, {type: 'NEXT_LYRIC'})).toEqual({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1
+});
+
+expect(reducer({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1,
+},
+  { type: 'RESTART_SONG'})).toEqual(initialState);
+
+//REDUX STORE
+const { createStore } = Redux;
+const store = createStore(reducer);
+
+
+//RENDERING STATE IN DOM
+const renderLyrics = () => {
+  // defines a lyricsDisplay constant referring to the div with a 'lyrics' ID in index.html
+  const lyricsDisplay = document.getElementById('lyrics');
+    // if there are already lyrics in this div, remove them one-by-one until it is empty:
+  while(lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  // Locates the song lyric at the current arrayPosition:
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  // Creates DOM text node containing the song lyric identified in line above:
+  const renderedLine = document.createTextNode(currentLine);
+  // Adds text node created in line above to 'lyrics' div in DOM
+  document.getElementById('lyrics').appendChild(renderedLine);
+}
+// runs renderLyrics() method from above when page is finished loading.
+// window.onload is HTML5 version of jQuery's $(document).ready()
+window.onload = function() {
+  renderLyrics();
+}
+//CLICK LISTENER
+const userClick = () => {
+  const currentState = store.getState();
+  if(currentState.arrayPosition === currentState.songLyricsArray.length-1) {
+    store.dispatch({ type: 'RESTART_SONG'});
+  } else {
+    store.dispatch({ type: 'NEXT_LYRIC'});
+  } 
+}
+
+// SUBSCRIBE TO REDUX STORE
+store.subscribe(renderLyrics);
